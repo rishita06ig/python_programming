986f0a472fc4b6b5f58738837c15aa5f288f5712
rishita06ig <rimic2006@gmail.com>
2026-03-31 12:42:11 +0530

    Add files via upload

---
+++ transpose.py
@@ -0,0 +1,15 @@
+A =[
+    [1,2,3],
+    [4,5,6]
+    ]
+###trans = [[A[i][j] for j in range(len(A))] for i in range(len(A[0]))]
+
+
+trans = []
+
+for i in range(len(A[0])):      
+    row = []
+    for j in range(len(A)):     
+        row.append(A[j][i])
+    trans.append(row)
+print(trans)
