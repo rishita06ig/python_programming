95da5bf0a7bff0430849a27a10ea67f0e4eb4266
rishita06ig <rimic2006@gmail.com>
2026-04-07 19:51:19 +0530

    Add files via upload

---
+++ matrix_multiplication.py
@@ -0,0 +1,33 @@
+import copy
+class mat:
+    def __init__(self,A):
+        self.nr = len(A)
+        self.nc = len(A[0])
+        self.data = copy.deepcopy(A)
+
+    def mul(self,b):
+        if self.nc == b.nr:
+            A = []
+            for i in range(self.nr):
+                Ci = []
+                for j in range(b.nc):
+                    s = 0
+                    for k in range(self.nc):
+                        s += self.data[i][k] * b.data[k][j]
+                    Ci.append(s)
+                A.append(Ci)
+            return mat(A)
+        else:
+            print("Multiplication not possible")
+    
+    def display(self):
+        for row in self.data:
+            print(row)
+
+A = [[4,5,6],[2,3,4]]
+B = [[1,8,5,2],[-1,5,6,2],[8,-2,3,7]]
+a=mat(A)
+b=mat(B)
+c=a.mul(b)
+print("Result of Multiplication :")
+c.display()
