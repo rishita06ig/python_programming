545c59e3b824cdf9f07fdbb0856bc0476e16b4f0
rishita06ig <rimic2006@gmail.com>
2026-04-07 19:50:09 +0530

    Delete mat_mul.py

--- mat_mul.py
+++
@@ -1,9 +0,0 @@
-import copy
-classs Mat:
-    def __init__(self,A):
-        self.nr = len(A)
-        self.nc=len(A[0])
-        self.data = copy.deepcopy(A)
-        
-    def mul(self,b):
-        
